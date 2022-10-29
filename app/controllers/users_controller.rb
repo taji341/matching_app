77bb35e5c326ff07efc084929e06f003a6a97392
taji341 <tajitaji341@gmail.com>
2022-10-29 21:47:40 +0900

    [add] LessonモデルCRUD作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,5 +10,25 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @lesson = @user.lesson
   end
-  
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "ユーザーの設定を編集しました"
+      redirect_to users_path
+    else
+      render "edit"
+      flash[:alert] = "エラー"
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :image, :gender, :address, :introduction)
+  end
 end
