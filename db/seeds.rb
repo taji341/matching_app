8e4e94e0ab445ba774801995311b2b857e0ab605
taji341 <tajitaji341@gmail.com>
2023-01-22 04:33:05 +0900

    [fix] テストデータ修正・追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,79 +1,129 @@
 # frozen_string_literal: true
 
-# 30.times {
-#   User.create!(
-#     name: Faker::Name.name,
-#     password: Faker::Internet.password(min_length: 6),
-#     email: Faker::Internet.free_email,
-#     image: File.open("./app/assets/images/test-user.jpeg"),
-#     address: Faker::Address.state,
-#     introduction: '----自己紹介----
-#                   ----自己紹介----
-#                   ----自己紹介----',
-#     gender: '1'
-#   )
-# }
+40.times {
+  User.create!(
+    name: Faker::Name.name,
+    password: Faker::Internet.password(min_length: 6),
+    email: Faker::Internet.free_email,
+    image: File.open("./app/assets/images/test-user.jpeg"),
+    address: Faker::Address.state,
+    introduction: '気軽にご連絡ください！
+                    よろしくお願いします。',
+    gender: '1'
+  )
+}
 
-# 20.times {
-#   User.create!(
-#     name: Faker::Name.name,
-#     password: Faker::Internet.password(min_length: 6),
-#     email: Faker::Internet.free_email,
-#     image: File.open("./app/assets/images/test-user.jpeg"),
-#     address: Faker::Address.state,
-#     introduction: '----自己紹介----
-#                   ----自己紹介----
-#                   ----自己紹介----',
-#     gender: '2'
-#   )
-# }
+45.times {
+  User.create!(
+    name: Faker::Name.name,
+    password: Faker::Internet.password(min_length: 6),
+    email: Faker::Internet.free_email,
+    image: File.open("./app/assets/images/test-user.jpeg"),
+    address: Faker::Address.state,
+    introduction: '気軽にご連絡ください！
+                    よろしくお願いします。',
+    gender: '2'
+  )
+}
 
-User.all.each do |user|
-#   Lesson.create!(
-#     title: 'テストレッスン',
-#     image: File.open("./app/assets/images/test-lesson.jpg"),
-#     address: Faker::Address.city,
-#     information: '---------------------レッスン紹介--------------------------
-#                   ---------------------レッスン紹介--------------------------
-#                   ---------------------レッスン紹介--------------------------
-#                   ---------------------レッスン紹介--------------------------
-#                   ---------------------レッスン紹介--------------------------
-#                   ---------------------レッスン紹介--------------------------',
-#     price: '1h/1000円〜',
-#     schedule: '毎週土日開催！気軽にご相談ください。',
-#     user_id: user.id
-#   )
+20.times do |n|
+  Lesson.create!(
+    title: 'ピアノレッスン(初心者歓迎!)',
+    image: File.open("./app/assets/images/top-piano.jpg"),
+    address: Faker::Address.city,
+    information: 'ピアノに関するレッスンです。
+                  初心者〜経験者まで気軽にどうぞ！
+                  オンライン可・出張可
+                  持ち物は特にありません。
+                  ご連絡お待ちしております。',
+    price: '1h/1000円〜',
+    schedule: '毎週土日開催！気軽にご相談ください。',
+    user_id: "#{n + 1}",
+  )
+end
 
-#   Favorite.create!(
-#     user_id: user.id,
-#     lesson_id: user.id + 1
-#   )
+20.times do |n|
+  Lesson.create!(
+    title: 'バイオリンレッスン(初心者歓迎!)',
+    image: File.open("./app/assets/images/top-violin.jpg"),
+    address: Faker::Address.city,
+    information: 'バイオリンに関するレッスンです。
+                  初心者〜経験者まで気軽にどうぞ！
+                  オンライン可・出張可
+                  持ち物は特にありません。
+                  ご連絡お待ちしております。',
+    price: '1h/1000円〜',
+    schedule: '毎週土日開催！気軽にご相談ください。',
+    user_id: "#{n + 21}"
+  )
+end
 
-#   Room.create!(
-#     name: user.name
-#   )
+20.times do |n|
+  Lesson.create!(
+    title: 'ギターレッスン(初心者歓迎!)',
+    image: File.open("./app/assets/images/top-guitar.jpg"),
+    address: Faker::Address.city,
+    information: 'ギターに関するレッスンです。
+                  初心者〜経験者まで気軽にどうぞ！
+                  オンライン可・出張可
+                  持ち物は特にありません。
+                  ご連絡お待ちしております。',
+    price: '1h/1000円〜',
+    schedule: '毎週土日開催！気軽にご相談ください。',
+    user_id: "#{n + 41}"
+  )
+end
 
-  Message.create!(
-    user_id: user.id + 1,
-    room_id: user.id,
-    content: '----------テストメッセージ------------
-                ----------テストメッセージ------------'
+22.times do |n|
+  Lesson.create!(
+    title: 'ドラムレッスン(初心者歓迎!)',
+    image: File.open("./app/assets/images/top-drum.jpg"),
+    address: Faker::Address.city,
+    information: 'ドラムに関するレッスンです。
+                  初心者〜経験者まで気軽にどうぞ！
+                  オンライン可・出張可
+                  持ち物は特にありません。
+                  ご連絡お待ちしております。',
+    price: '1h/1000円〜',
+    schedule: '毎週土日開催！気軽にご相談ください。',
+    user_id: "#{n + 61}"
+  )
+end
+
+80.times do |n|
+  Favorite.create!(
+    user_id: "#{n + 1}",
+    lesson_id: "#{n + 2}"
+  )
+
+  Room.create!(
+    name: "#{n + 1}"
   )
 
   Message.create!(
-    user_id: user.id,
-    room_id: user.id,
-    content: '----------テストメッセージ------------
-                ----------テストメッセージ------------'
+    user_id: "#{n + 1}",
+    room_id: "#{n + 1}",
+    content: '初めまして！
+              今週土日どちらか予約できないでしょうか？
+              オンラインを希望です！
+              よろしくお願いします。'
+  )
+
+  Message.create!(
+    user_id: "#{n + 2}",
+    room_id: "#{n + 1}",
+    content: '初めまして！
+              土曜日13時〜18時でしたらいつでも大丈夫です!
+              お時間決まりましたら再度ご連絡お願いします。'
   )
 
   RoomUser.create!(
-    user_id: user.id + 1,
-    room_id: user.id
+    user_id: "#{n + 1}",
+    room_id: "#{n + 1}"
   )
 
   RoomUser.create!(
-    user_id: user.id,
-    room_id: user.id
+    user_id: "#{n + 2}",
+    room_id: "#{n + 1}"
   )
 end
