932e76c812e4e627dd34b8d150d4c2aca2af521f
taji341 <tajitaji341@gmail.com>
2023-01-23 18:18:38 +0900

    [fix] rspec　画像参照の方法を変更

--- spec/factories/lessons.rb
+++ spec/factories/lessons.rb
@@ -3,7 +3,7 @@
 FactoryBot.define do
   factory :lesson do
     title { 'テストレッスン' }
-    image { Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/test-lesson.jpg')) }
+    image { File.open("#{Rails.root}/spec/fixtures/test-lesson.jpg") }
     information { 'テストレッスンの説明' }
     address { '東京都' }
     price { '1h/1000' }
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,32 +5,25 @@ FactoryBot.define do
     name { 'テストユーザー' }
     email { 'test@example.com' }
     password { 'password' }
-    image { Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/test-user.jpeg')) }
+    image { File.open("#{Rails.root}/spec/fixtures/test-user.jpeg") }
     address { '東京都' }
     introduction { 'テストユーザー' }
     gender { '1' }
+
+    factory :user1 do
+      email { 'test1@example.com' }
+      gender { '2' }
+    end
+
+    factory :user3 do
+      email { 'test3@example.com' }
+      gender { '2' }
+    end
   end
-  factory :user1, class: User do
-    name { 'テストユーザー' }
-    email { 'test1@example.com' }
-    password { 'password' }
-    image { Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/test-user.jpeg')) }
-    address { '東京都' }
-    introduction { 'テストユーザー' }
-    gender { '2' }
-  end
+
   factory :user2, class: User do
     name { 'テストユーザー' }
     email { 'test2@example.com' }
     password { 'password' }
-  end
-  factory :user3, class: User do
-    name { 'テストユーザー' }
-    email { 'test3@example.com' }
-    password { 'password' }
-    image { Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/test-user.jpeg')) }
-    address { '東京都' }
-    introduction { 'テストユーザー' }
-    gender { '2' }
   end
 end
